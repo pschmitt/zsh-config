1d32f63a77004f042dfc72c88c8294455b1e6300
Philipp Schmitt <philipp@schmitt.co>
2015-11-03 18:59:44 +0100

    Update fzf

--- plugins/fzf.zsh
+++ plugins/fzf.zsh
@@ -1,66 +1,20 @@
-# Setup fzf function
-# ------------------
-unalias fzf 2> /dev/null
-fzf() {
-  /usr/bin/ruby $ZDOTDIR/plugins/fzf/fzf "$@"
-}
-export -f fzf > /dev/null
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/pschmitt/.config/zsh/plugins/fzf/bin* ]]; then
+  export PATH="$PATH:/home/pschmitt/.config/zsh/plugins/fzf/bin"
+fi
+
+# Man path
+# --------
+if [[ ! "$MANPATH" == */home/pschmitt/.config/zsh/plugins/fzf/man* && -d "/home/pschmitt/.config/zsh/plugins/fzf/man" ]]; then
+  export MANPATH="$MANPATH:/home/pschmitt/.config/zsh/plugins/fzf/man"
+fi
 
 # Auto-completion
 # ---------------
-# [[ $- =~ i ]] && source $ZDOTDIR/plugins/fzf/fzf-completion.zsh
+[[ $- == *i* ]] && source "/home/pschmitt/.config/zsh/plugins/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-# CTRL-T - Paste the selected file path(s) into the command line
-__fsel() {
-  set -o nonomatch
-  command find * -path '*/\.*' -prune \
-    -o -type f -print \
-    -o -type d -print \
-    -o -type l -print 2> /dev/null | fzf -m | while read item; do
-    printf '%q ' "$item"
-  done
-  echo
-}
+source "/home/pschmitt/.config/zsh/plugins/fzf/shell/key-bindings.zsh"
 
-if [[ $- =~ i ]]; then
-
-if [ -n "$TMUX_PANE" -a ${FZF_TMUX:-1} -ne 0 -a ${LINES:-40} -gt 15 ]; then
-  fzf-file-widget() {
-    local height
-    height=${FZF_TMUX_HEIGHT:-40%}
-    if [[ $height =~ %$ ]]; then
-      height="-p ${height%\%}"
-    else
-      height="-l $height"
-    fi
-    tmux split-window $height "zsh -c 'source $ZDOTDIR/plugins/fzf.zsh; tmux send-keys -t $TMUX_PANE \"\$(__fsel)\"'"
-  }
-else
-  fzf-file-widget() {
-    LBUFFER="${LBUFFER}$(__fsel)"
-    zle redisplay
-  }
-fi
-zle     -N   fzf-file-widget
-bindkey '^T' fzf-file-widget
-
-# ALT-K - cd into the selected directory
-fzf-cd-widget() {
-  cd "${$(set -o nonomatch; command find * -path '*/\.*' -prune \
-          -o -type d -print 2> /dev/null | fzf):-.}"
-  zle reset-prompt
-}
-zle     -N    fzf-cd-widget
-bindkey '^[k' fzf-cd-widget
-
-# CTRL-R - Paste the selected command from history into the command line
-fzf-history-widget() {
-  LBUFFER=$(fc -l 1 | fzf +s +m -n..,1,2.. | sed "s/ *[0-9*]* *//")
-  zle redisplay
-}
-zle     -N   fzf-history-widget
-bindkey '^R' fzf-history-widget
-
-fi
